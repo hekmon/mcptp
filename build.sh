2041df1b876fb1d08422bc4cf9026fd5ff04a0d4
Hekmon <edouardhur@gmail.com>
2026-05-13 12:11:05 +0200

    Add cross-platform build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+version=$(git describe --tags --always --dirty)
+
+crosscompile () {
+    if [ "$1" == "windows" ]; then
+        name='mcptp.exe'
+    else
+        name='mcptp'
+    fi
+    GOOS="$1" GOARCH="$2" go build -ldflags="-s -w -X 'main.Version=${version}'" -o "$name"
+    zip -9 "mcptp_${version}_${1}_${2}.zip" "$name"
+}
+
+echo '* Compiling for Windows'
+crosscompile 'windows' 'amd64'
+echo
+echo '* Compiling for MacOS Intel'
+crosscompile 'darwin' 'amd64'
+echo
+echo '* Compiling for MacOS Apple Silicon'
+crosscompile 'darwin' 'arm64'
+echo
+echo '* Compiling for Linux'
+crosscompile 'linux' 'amd64'
+echo
+echo '* Cleaning up'
+test -f mcptp && rm mcptp
+test -f mcptp.exe && rm mcptp.exe
